8d44a3ba0c730adfcea16c4759de057f502bb393
Miguel Ordonez Camara <miguelordonezcamara@hotmail.com>
2026-08-13 23:02:10 -0600

    Update JwtProperties

--- src/main/java/com/teslo/shop/config/JwtProperties.java
+++ src/main/java/com/teslo/shop/config/JwtProperties.java
@@ -6,7 +6,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 public class JwtProperties {
 
     private String secret;
-    private long expiration = 7200;
+    private long expiration;
 
     public String getSecret() {
         return secret;
