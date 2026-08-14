ee46937a49719aeb439dbd96432e152e0eab179d
Miguel Ordonez Camara <miguelordonezcamara@hotmail.com>
2026-08-14 08:09:25 -0600

    Update JwtService

--- src/main/java/com/teslo/shop/auth/jwt/JwtService.java
+++ src/main/java/com/teslo/shop/auth/jwt/JwtService.java
@@ -5,15 +5,10 @@ import com.teslo.shop.config.JwtProperties;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
-// import io.jsonwebtoken.io.Decoders;
-// import io.jsonwebtoken.security.Keys;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
-
-// import java.nio.charset.StandardCharsets;
 import java.util.Date;
 import javax.crypto.SecretKey;
-// import javax.crypto.spec.SecretKeySpec;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -23,26 +18,28 @@ public class JwtService {
     private final long expirationSeconds;
 
     public JwtService(JwtProperties properties) {
+        // TODO: Op. 1
         // this.key = Jwts.SIG.HS256.key().build();
-
+        // TODO: Op. 2
+        // this.key = new SecretKeySpec(
+        //     properties.getSecret().getBytes(StandardCharsets.UTF_8),
+        //     "HmacSHA256"
+        // );
+        // TODO: Op. 3
         byte[] keyBytes = Decoders.BASE64.decode(properties.getSecret());
         this.key = Keys.hmacShaKeyFor(keyBytes);
-
-
-        // this.key = new SecretKeySpec(
-        //         properties.getSecret().getBytes(StandardCharsets.UTF_8),
-        //         "HmacSHA256");
         this.expirationSeconds = properties.getExpiration();
     }
 
     public String generateToken(User user) {
-
         return Jwts.builder()
-                .claim("id", user.getId().toString())
-                .issuedAt(new Date())
-                .expiration(new Date(System.currentTimeMillis() + expirationSeconds * 1000))
-                .signWith(key)
-                .compact();
+            .claim("id", user.getId().toString())
+            .issuedAt(new Date())
+            .expiration(
+                new Date(System.currentTimeMillis() + expirationSeconds * 1000)
+            )
+            .signWith(key)
+            .compact();
     }
 
     public String extractUserId(String token) {
@@ -60,9 +57,9 @@ public class JwtService {
 
     private Claims parse(String token) {
         return Jwts.parser()
-                .verifyWith(key)
-                .build()
-                .parseSignedClaims(token)
-                .getPayload();
+            .verifyWith(key)
+            .build()
+            .parseSignedClaims(token)
+            .getPayload();
     }
 }
